c56167d7bcbde66b5b139b2c6b1637fb0a528799
Komal <Komal@KOMAL-PC>
2022-05-12 15:34:38 +0530

    First Commit...

---
+++ src/Test1.java
@@ -0,0 +1,17 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String action="hungry";
+		if(action=="hungry"){
+		
+			System.out.println("eat");
+		}else{
+			System.out.println("work");
+		}
+
+	}
+
+}
